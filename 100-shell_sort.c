ecabb1f7898c7eb0cf7389d4ead7b47fe37aa866
Willmuseve <willmuseve@gmail.com>
2023-07-01 19:38:59 -0700

    Knuth sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -6,31 +6,43 @@
   * @size: length of the array.
   * Return: Nothing.
   */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * shell_sort - Sort an array of integers in ascending
+ *              order using the shell sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Knuth interval sequence.
+ */
 void shell_sort(int *array, size_t size)
 {
-	unsigned int x = 0, j = 0, gap = 0;
-	int aux = 0;
+	size_t gap, i, j;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (gap < size / 3)
+	for (gap = 1; gap < (size / 3);)
 		gap = gap * 3 + 1;
 
-	for (; gap > 0; gap = (gap - 1) / 3)
+	for (; gap >= 1; gap /= 3)
 	{
-		for (x = gap; x < size; x++)
+		for (i = gap; i < size; i++)
 		{
-			aux = array[x];
-			for (j = x; j >= gap && array[j - gap] > aux;
-					j -= gap)
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
 			{
-				if (array[j] != array[j - gap])
-					array[j] = array[j - gap];
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
 			}
-			if (array[j] != aux)
-				array[j] = aux;
-
 		}
 		print_array(array, size);
 	}
